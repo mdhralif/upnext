9cea46cc73de0a5452dc7b5438fa5fd8b76f8dfb
mdhralif <hasiburrahman999.alif@gmail.com>
2025-07-14 23:12:40 +0600

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://upnext-frontend.vercel.app/'] 
+    ? ['https://upnext-frontend.vercel.app'] 
     : ['http://localhost:3000'],
   credentials: true
 }));
